63a418054a0f16763999d07744a356e12ff1da59
Thiago Comba <136867668+Thcmb@users.noreply.github.com>
2025-07-21 10:18:14 -0300

    Update index.tsx

--- src/pages/Entry/index.tsx
+++ src/pages/Entry/index.tsx
@@ -17,10 +17,17 @@ const Entry = () => (
           <AccountAction />
         </SectionLoguin>
       </SplitLayout>
+
       <Footer>
         <p>
           Todos direitos reservados{' '}
-          <a href="https://github.com/Thcmb" target="_blank" rel="noreferrer">@Thcmb</a>
+          <a
+            href="https://github.com/Thcmb"
+            target="_blank"
+            rel="noreferrer"
+          >
+            @Thcmb
+          </a>
         </p>
       </Footer>
     </div>
